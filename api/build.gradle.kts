c7634fd759b7be60519ca5e3222b4a9743f24c90
liu.hua <liu.hua@tineco.com>
2025-06-01 14:23:43 +0800

    use buildSrc share build configure

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,29 +1,7 @@
 plugins {
-    `java-library`
-}
-
-repositories {
-
-    maven {
-        url = uri("https://maven.aliyun.com/repository/public")
-    }
-
-    mavenCentral()
+    `java-common-conventions`
 }
 
 dependencies {
-    implementation("org.slf4j:slf4j-api:2.0.9")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.11.3")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
-
-tasks.named<Test>("test") {
-    useJUnitPlatform()
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,29 +1,8 @@
 plugins {
-    `java-library`
-}
-
-repositories {
-
-    maven {
-        url = uri("https://maven.aliyun.com/repository/public")
-    }
-
-    mavenCentral()
+    `java-common-conventions`
 }
 
 dependencies {
-    implementation("org.slf4j:slf4j-api:2.0.9")
     implementation("com.google.guava:guava:32.1.1-jre")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.11.3")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
-
-tasks.named<Test>("test") {
-    useJUnitPlatform()
-}
