2bd9b2e1264534a6a06816a7c07aba0900935bf7
liu.hua <liu.hua@tineco.com>
2025-06-01 13:41:21 +0800

    init

---
+++ api/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    `java-library`
+}
+
+repositories {
+
+    maven {
+        url = uri("https://maven.aliyun.com/repository/public")
+    }
+
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.slf4j:slf4j-api:2.0.9")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
